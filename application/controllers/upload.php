6c89a2acdd6120db1af1205f079803129bb17065
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-18 14:37:07 +0800

    Update upload.php

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -1,15 +1,15 @@
 <?php
 
-class Upload extends Controller {
-	
-	function Upload()
+class Upload extends CI_Controller {
+
+	function __construct()
 	{
-		parent::Controller();
+		parent::__construct();
 		$this->load->helper(array('form', 'url'));
 	}
-	
+
 	function index()
-	{	
+	{
 		$this->load->view('upload_form', array('error' => ' ' ));
 	}
 
@@ -20,21 +20,21 @@ class Upload extends Controller {
 		$config['max_size']	= '100';
 		$config['max_width']  = '1024';
 		$config['max_height']  = '768';
-		
+
 		$this->load->library('upload', $config);
-	
+
 		if ( ! $this->upload->do_upload())
 		{
 			$error = array('error' => $this->upload->display_errors());
-			
+
 			$this->load->view('upload_form', $error);
-		}	
+		}
 		else
 		{
 			$data = array('upload_data' => $this->upload->data());
-			
+
 			$this->load->view('upload_success', $data);
 		}
-	}	
+	}
 }
 ?>
