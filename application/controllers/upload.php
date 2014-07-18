fdecdd3864361a33008d232f0e38cb61d045564a
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-18 11:58:09 +0800

    Update upload.php

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -23,14 +23,8 @@ class Upload extends CI_Controller {
 		if ( ! $this->upload->do_upload())
 		{
 			$error = array('error' => $this->upload->display_errors());
+		}
 			$this->load->view('upload_form', $error);
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-
-			$this->load->view('upload_success', $data);
-		}
 	}
 }
 ?>
