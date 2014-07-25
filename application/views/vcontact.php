cad5a5ce61f2c8e50b53bbf2c9e17490e81dbbdb
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-25 10:18:58 +0800

    Update vcontact.php

--- application/views/vcontact.php
+++ application/views/vcontact.php
@@ -18,6 +18,51 @@ padding: 2px 8px;
 margin-top: 2px;
 }
 </style>
+<script>
+$(document).ready(function() {
+ $('#contact-form').validate({
+  rules: {
+   name: {
+    minlength: 2,
+    required: true
+   },
+   email: {
+    required: true,
+    email: true
+   },
+   subject: {
+    minlength: 2,
+    required: true
+   },
+   message: {
+    minlength: 2,
+    required: true
+   }
+  },
+  highlight: function(element) {
+   $(element).closest('.control-group').removeClass('success').addClass('error');
+  },
+  success: function(element) {
+   element.text('OK!').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
+  }, 
+  
+  submitHandler: function( form ) {
+       
+        $.ajax({
+            url : 'contact/process',
+            data : $('#contact-form').serialize(),
+            type: "POST",
+            success : function(data){
+             $("#contact-form").hide('slow');
+             $('#results').html(data);
+            }
+        })
+        return false;
+     }
+  
+ });  
+}); // end document.ready
+</script>
 <div class="container-fluid">
 <div class="row-fluid">
 <div class="span9 offset3">
